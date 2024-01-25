f1c319f9a634b821296eccb598cd34cca44b2093
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-01-25 13:45:29 +0600

    catch token

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar, StyleSheet, Text, View } from "react-native";
 import Login from "./App/Screens/LoginScreen/Login";
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import Constants from "expo-constants";
+import * as SecureStore from "expo-secure-store";
 
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 export default function App() {
   return (
-    <ClerkProvider publishableKey="pk_test_ZmFjdHVhbC1kaW5vc2F1ci03MS5jbGVyay5hY2NvdW50cy5kZXYk">
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey={Constants.expoConfig.extra.clerkPublishableKey}
+    >
       <View style={styles.container}>
         <SignedIn>
           <Text>You are Signed in</Text>
